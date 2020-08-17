9c8a3e109fbbf0ed16b1cec876f41c0e9aaf97ce
Christoph Welcz <christoph@welcz.de>
2020-08-18 00:32:35 +0200

    :white_check_mark: implementing multiple csv lines

--- src/Csv.hs
+++ src/Csv.hs
@@ -6,9 +6,14 @@ import Text.Parsec.Language (emptyDef)
 import Text.Parsec.String (Parser)
 import qualified Text.Parsec.Token as Token
 
-data CsvFile = CsvFile CsvHeaders [CsvLine] deriving (Show, Eq)
+data CsvFile = CsvFile
+  { headers :: CsvHeaders,
+    lines :: [CsvLine]
+  }
+  deriving (Show, Eq)
 
 type CsvHeaders = CsvLine
+
 newtype CsvLine = CsvLine [String] deriving (Show, Eq)
 
 csvFile :: Parser CsvFile
@@ -17,10 +22,14 @@ csvFile = f <$ many anyChar <* eof
     f = CsvFile (CsvLine []) [CsvLine []]
 
 csvLine :: Parser CsvLine
-csvLine = CsvLine <$> sepBy csvColumn (spaces *> char ',' *> spaces)
+csvLine = CsvLine <$> sepBy csvColumn (try (spaces *> char ',' *> spaces))
+
+csvLines :: Parser [CsvLine]
+csvLines = sepBy csvLine endOfLine
 
 csvColumn :: Parser String
-csvColumn = stringLiteral <|> many (noneOf " ,")
+csvColumn = stringLiteral <|> many (noneOf " ,\r\n")
+--csvColumn = stringLiteral <|> many alphaNum
 
 tokenParser :: Token.GenTokenParser String u Identity
 tokenParser = Token.makeTokenParser emptyDef
--- test/CsvSpec.hs
+++ test/CsvSpec.hs
@@ -16,7 +16,7 @@ main = hspec spec
 spec :: Spec
 spec =
   describe "CSV Parser" $ do
-    context "parsing lines" $ do
+    context "parsing a line" $ do
       it "parses single column" $
         regularParse csvLine "first" `shouldBe` Right (CsvLine ["first"])
       it "parses comma separated multiple columns" $
@@ -25,9 +25,14 @@ spec =
         regularParse csvLine "first       , second" `shouldBe` Right (CsvLine ["first", "second"])
       it "parses quoted columns" $
         regularParse csvLine "\"first\",\"second,third\"" `shouldBe` Right (CsvLine ["first", "second,third"])
+    context "parsing multiple lines" $ do
+      it "parses two lines" $
+        regularParse csvLines "first,second\nthird,fourth" `shouldBe` Right [CsvLine ["first", "second"], CsvLine ["third", "fourth"]]
+      it "parses two lines in windows" $
+        regularParse csvLines "first,second\r\nthird,fourth" `shouldBe` Right [CsvLine ["first", "second"], CsvLine ["third", "fourth"]]
     context "parsing whole file" $ do
       it "parses empty file" $
-        parseFromFile csvFile "example-files/empty.csv" `shouldReturn` Right (CsvFile (CsvLine []) [CsvLine []])
+        parseFromFile csvFile "example-files/empty.csv" `shouldReturn` Right (CsvFile {headers = CsvLine [], Csv.lines = [CsvLine []]})
       xit "parses file with single column" $
         parseFromFile csvFile "example-files/single-column.csv"
           `shouldReturn` Right
