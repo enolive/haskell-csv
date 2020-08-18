c3d85ce43aed80130318c8d78946e438c52d8679
Christoph Welcz <christoph@welcz.de>
2020-08-18 02:05:57 +0200

    finished exercise

--- src/Csv.hs
+++ src/Csv.hs
@@ -5,31 +5,51 @@ import Text.Parsec
 import Text.Parsec.Language (emptyDef)
 import Text.Parsec.String (Parser)
 import qualified Text.Parsec.Token as Token
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 data CsvFile = CsvFile
   { headers :: CsvHeaders,
-    lines :: [CsvLine]
+    content :: [CsvColumns]
   }
   deriving (Show, Eq)
 
 type CsvHeaders = CsvLine
+type CsvColumns = Map String String
 
 newtype CsvLine = CsvLine [String] deriving (Show, Eq)
 
 csvFile :: Parser CsvFile
-csvFile = f <$ many anyChar <* eof
+csvFile = f <$> csvLines <* spaces <* eof
   where
-    f = CsvFile (CsvLine []) [CsvLine []]
+    f [] = CsvFile {headers = CsvLine [], content = []}
+    f (x:xs) = CsvFile {headers = x, content = buildAll x xs}
 
-csvLine :: Parser CsvLine
-csvLine = CsvLine <$> sepBy csvColumn (try (spaces *> char ',' *> spaces))
+buildAll :: CsvHeaders -> [CsvLine] -> [CsvColumns]
+buildAll h ls = buildOne h <$> ls
+
+buildOne :: CsvHeaders -> CsvLine -> CsvColumns
+buildOne (CsvLine hs) (CsvLine cols) = foldl (tryInsert hs) initialMap (zip cols [0..])
+  where
+    initialMap = Map.fromList (zip hs (repeat ""))
+
+tryInsert :: [String] -> CsvColumns -> (String, Int) -> CsvColumns
+tryInsert hs cols (col,index)
+  | length hs > index = Map.insert (hs !! index) col cols
+  | otherwise = cols
 
 csvLines :: Parser [CsvLine]
-csvLines = sepBy csvLine endOfLine
+csvLines = removeTrailing <$> sepBy csvLine endOfLine
+  where
+    removeTrailing [] = []
+    removeTrailing [CsvLine [""]] = []
+    removeTrailing (x:xs) = x : removeTrailing xs
+
+csvLine :: Parser CsvLine
+csvLine = CsvLine <$> sepBy csvColumn (char ',')
 
 csvColumn :: Parser String
-csvColumn = stringLiteral <|> many (noneOf " ,\r\n")
---csvColumn = stringLiteral <|> many alphaNum
+csvColumn = stringLiteral <|> many (noneOf ",\r\n")
 
 tokenParser :: Token.GenTokenParser String u Identity
 tokenParser = Token.makeTokenParser emptyDef
--- test/CsvSpec.hs
+++ test/CsvSpec.hs
@@ -6,6 +6,7 @@ module CsvSpec
 where
 
 import Csv
+import qualified Data.Map as Map
 import ParseFunctions
 import Test.Hspec
 import Text.Parsec.String
@@ -21,22 +22,64 @@ spec =
         regularParse csvLine "first" `shouldBe` Right (CsvLine ["first"])
       it "parses comma separated multiple columns" $
         regularParse csvLine "first,second,third" `shouldBe` Right (CsvLine ["first", "second", "third"])
-      it "ignores whitespaces between columns" $
-        regularParse csvLine "first       , second" `shouldBe` Right (CsvLine ["first", "second"])
       it "parses quoted columns" $
         regularParse csvLine "\"first\",\"second,third\"" `shouldBe` Right (CsvLine ["first", "second,third"])
     context "parsing multiple lines" $ do
+      it "parses single line" $
+        regularParse csvLines "only line" `shouldBe` Right [CsvLine ["only line"]]
       it "parses two lines" $
         regularParse csvLines "first,second\nthird,fourth" `shouldBe` Right [CsvLine ["first", "second"], CsvLine ["third", "fourth"]]
+      it "ignores empty trailing line" $
+        regularParse csvLines "first\nsecond\n" `shouldBe` Right [CsvLine ["first"], CsvLine ["second"]]
       it "parses two lines in windows" $
         regularParse csvLines "first,second\r\nthird,fourth" `shouldBe` Right [CsvLine ["first", "second"], CsvLine ["third", "fourth"]]
+    context "building content map" $ do
+      it "works" $
+        buildAll (CsvLine ["column1", "column2"]) [CsvLine ["first1", "first2"], CsvLine ["second1", "second2"]]
+          `shouldBe` [ Map.fromList [("column1", "first1"), ("column2", "first2")],
+                       Map.fromList [("column1", "second1"), ("column2", "second2")]
+                     ]
+      it "ignores content without column" $
+        buildAll (CsvLine ["column1"]) [CsvLine ["first1", "first2"]]
+          `shouldBe` [Map.fromList [("column1", "first1")]]
+      it "fills columns without content" $
+        buildAll (CsvLine ["column1", "column2"]) [CsvLine ["first1"]]
+          `shouldBe` [Map.fromList [("column1", "first1"), ("column2", "")]]
     context "parsing whole file" $ do
       it "parses empty file" $
-        parseFromFile csvFile "example-files/empty.csv" `shouldReturn` Right (CsvFile {headers = CsvLine [], Csv.lines = [CsvLine []]})
-      xit "parses file with single column" $
+        parseFromFile csvFile "example-files/empty.csv" `shouldReturn` Right (CsvFile {headers = CsvLine [], content = []})
+      it "parses file with single column" $
         parseFromFile csvFile "example-files/single-column.csv"
           `shouldReturn` Right
             ( CsvFile
-                (CsvLine ["column"])
-                [CsvLine ["first", "second", "third"]]
+                { headers = CsvLine ["column"],
+                  content =
+                    [ Map.fromList [("column", "first")],
+                      Map.fromList [("column", "second")],
+                      Map.fromList [("column", "third")]
+                    ]
+                }
+            )
+      it "parses file with multiple columns" $
+        parseFromFile csvFile "example-files/multiple-columns.csv"
+          `shouldReturn` Right
+            ( CsvFile
+                { headers = CsvLine ["column1", "column2"],
+                  content =
+                    [ Map.fromList [("column1", "first1"), ("column2", "first2")],
+                      Map.fromList [("column1", "second1"), ("column2", "second2")],
+                      Map.fromList [("column1", "third1"), ("column2", "third2")]
+                    ]
+                }
+            )
+      it "parses file with uneven structure" $
+        parseFromFile csvFile "example-files/uneven.csv"
+          `shouldReturn` Right
+            ( CsvFile
+                { headers = CsvLine ["first", "second"],
+                  content =
+                    [ Map.fromList [("first", "1"), ("second", "2")],
+                      Map.fromList [("first", "1"), ("second", "")]
+                    ]
+                }
             )
